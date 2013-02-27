97666249baa64f00d5bf555c949fca36aea147d8
Nat Welch <nat@natwelch.com>
2013-02-26 17:58:58 -0800

    base site.

---
+++ site.rb
@@ -0,0 +1,13 @@
+enable  :sessions, :logging
+
+set :session_secret, ENV['SESSION_SECRET'] || 'This is a secret bro!'
+
+set :root, File.dirname(__FILE__)
+
+get '/' do
+  erb :index
+end
+
+post '/' do
+  ""
+end
